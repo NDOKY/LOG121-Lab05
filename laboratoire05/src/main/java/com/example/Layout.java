06058dc3fac3a788aabace5da2a2f4a66aad4572
Alexander Pan <115491828+Panzers845@users.noreply.github.com>
2024-07-21 15:24:18 -0400

    Le layout esst fait forreal forreal

--- laboratoire05/src/main/java/com/example/Layout.java
+++ laboratoire05/src/main/java/com/example/Layout.java
@@ -3,15 +3,12 @@ package com.example;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.ScrollEvent;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class Layout {
-
-    private double zoomFactor = 1.0;
 
     public void build(Stage primaryStage) {
         primaryStage.setTitle("Image Editor");
@@ -35,6 +32,9 @@ public class Layout {
 
         // Presse Papier Menu
         Menu pressePapierMenu = new Menu("Presse Papier");
+        MenuItem copyItem = new MenuItem("Copy");
+        MenuItem pasteItem = new MenuItem("Paste");
+        pressePapierMenu.getItems().addAll(copyItem, pasteItem);
 
         menuBar.getMenus().addAll(fichierMenu, editionMenu, pressePapierMenu);
 
@@ -43,39 +43,19 @@ public class Layout {
         borderPane.setTop(menuBar);
 
         GridPane gridPane = new GridPane();
-        ImageView fixedImageView = new ImageView();
-        ImageView imageView1 = new ImageView();
-        ImageView imageView2 = new ImageView();
 
-        gridPane.add(fixedImageView, 0, 0);
-        gridPane.add(imageView1, 1, 0);
-        gridPane.add(imageView2, 2, 0);
+        // Set column constraints
+        ColumnConstraints column1 = new ColumnConstraints();
+        column1.setPercentWidth(33.33);
+        ColumnConstraints column2 = new ColumnConstraints();
+        column2.setPercentWidth(33.33);
+        ColumnConstraints column3 = new ColumnConstraints();
+        column3.setPercentWidth(33.33);
+
+        gridPane.getColumnConstraints().addAll(column1, column2, column3);
+
 
         borderPane.setCenter(gridPane);
-
-        // Zoom and Move Handlers
-        gridPane.addEventFilter(ScrollEvent.SCROLL, event -> {
-            if (event.getDeltaY() > 0) {
-                zoomFactor *= 1.1;
-            } else {
-                zoomFactor /= 1.1;
-            }
-            fixedImageView.setScaleX(zoomFactor);
-            fixedImageView.setScaleY(zoomFactor);
-            imageView1.setScaleX(zoomFactor);
-            imageView1.setScaleY(zoomFactor);
-            imageView2.setScaleX(zoomFactor);
-            imageView2.setScaleY(zoomFactor);
-        });
-
-        gridPane.addEventFilter(MouseEvent.MOUSE_DRAGGED, event -> {
-            fixedImageView.setTranslateX(event.getX());
-            fixedImageView.setTranslateY(event.getY());
-            imageView1.setTranslateX(event.getX());
-            imageView1.setTranslateY(event.getY());
-            imageView2.setTranslateX(event.getX());
-            imageView2.setTranslateY(event.getY());
-        });
 
         Scene scene = new Scene(borderPane, 800, 600);
         primaryStage.setScene(scene);
